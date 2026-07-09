024f06795464f0c6b0dc8c0a7050259a6a187003
Kevin Tourret <kevin@drosalys.fr>
2026-07-09 14:14:49 +0200

    add partial/component example

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Repository\CategoryRepository;
 use App\Repository\GameRepository;
+use App\Repository\ReviewRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -10,12 +12,24 @@ use Symfony\Component\Routing\Attribute\Route;
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function home(GameRepository $gameRepository): Response
+    public function home(
+        GameRepository     $gameRepository,
+        ReviewRepository   $reviewRepository,
+        CategoryRepository $categoryRepository,
+    ): Response
     {
-        $games = $gameRepository->findAll();
+        $trends = $gameRepository->findBy([], ['publishedAt' => 'DESC'], 9);
+        $bests = $gameRepository->findBy([], ['price' => 'DESC'], 9);
+        $reviews = $reviewRepository->findBy(['rating' => 5], ['createdAt' => 'DESC'], 5);
+        $tops = $gameRepository->findBy([], ['name' => 'ASC'], 6);
+        $categories = $categoryRepository->findBy([], ['name' => 'ASC'], 9);
 
         return $this->render('front/home/index.html.twig', [
-            'games' => $games,
+            'trends' => $trends,
+            'bests' => $bests,
+            'reviews' => $reviews,
+            'tops' => $tops,
+            'categories' => $categories,
         ]);
     }
 }
