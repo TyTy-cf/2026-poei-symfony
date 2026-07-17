b86748716543d7c1256606602668e2ae697c5a21
BarbierQ <barbierquentin@hotmail.com>
2026-07-17 14:38:16 +0200

    Hibou

--- src/Controller/Admin/CategoryController.php
+++ src/Controller/Admin/CategoryController.php
@@ -2,7 +2,12 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Category;
+use App\Form\RegisterType;
+use App\Service\SlugifyService;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -19,11 +24,29 @@ final class CategoryController extends AbstractController
     }
 
     #[Route('new', name: 'new')]
-    public function new(): Response
+    public function new(
+        Request                $request,
+        EntityManagerInterface $em,
+//        SlugifyService         $slugifyService,
+    ): Response
     {
+        $category = new Category();
+        $form = $this->createForm(RegisterType::class, $category);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $name = $form->getData()->getName();
+            $category->setSlug($slugifyService->slugify($name));
+            try {
+                $em->persist($category);
+                $em->flush();
+                $this->addFlash("success", "c'est ajouté en base");
+            } catch (\Exception $e) {
+                $this->addFlash("error", "et bah non");
+            }
+        }
         return $this->render('admin/category/edit.html.twig', [
             'form' => $form,
         ]);
     }
-
 }
