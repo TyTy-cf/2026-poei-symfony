693bad50c379886638e43d9a71756da3e1de1039
BriceVaillant <naw.brice@gmail.com>
2026-07-20 08:54:22 +0200

    all fixed

---
+++ src/Controller/Admin/AdminController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Repository\CategoryRepository;
+use App\Repository\GameRepository;
+use App\Repository\ReviewRepository;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
+
+use Symfony\Component\Routing\Attribute\Route;
+
+final class AdminController extends AbstractController
+{
+
+  #[Route('/{_locale}/admin/dashboard/', name: 'admin_dashboard')]
+  public function index(GameRepository $gameRepository, ReviewRepository $reviewRepository, CategoryRepository $categoryRepository): Response
+  {
+
+    //doit être les 9 derniers jeux sortis
+    $latestReleasedGames = $gameRepository->findLatestReleases(9);
+
+    // les 6 jeux avec le meilleur rating
+    $bestRatedGames = $gameRepository->findByPopularity(6);
+
+    $latestReviews = $reviewRepository->findBy([], ["upvote" => "DESC"], 6);
+
+    // les 9 jeux les plus joués (Query custom !)
+    $mostPlayedGames = $gameRepository->mostPlayedGames(9);
+
+    $categories = $categoryRepository->findBy([], ["name" => "ASC"], 9);
+
+    $mostPlayedCategories = $categoryRepository->mostPlayedCategories(5);
+
+    // dd($mostPlayedCategories);
+    return $this->render('admin/home/index.html.twig', [
+      'controller_name' => 'HomeController',
+      'latestReleasedGames' => $latestReleasedGames,
+      'latestReviews' => $latestReviews,
+      'bestRatedGames' => $bestRatedGames,
+      'categories' => $categories,
+      'mostPlayedGames' => $mostPlayedGames,
+      'mostPlayedCategories' => $mostPlayedCategories,
+    ]);
+  }
+}
