6bb3cf86cc218d7fc64894c22b3c1691989bf236
Stéphane Leite <sleite1910@gmail>
2026-07-21 11:12:08 +0200

    form review

--- src/Controller/App/GameController.php
+++ src/Controller/App/GameController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller\App;
 
+use App\Entity\Review;
+use App\Form\ReviewType;
 use App\Repository\GameRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -12,9 +16,11 @@ final class GameController extends AbstractController
 {
     #[Route('/{_locale}/game/{slug}', name: 'app_game_show')]
     public function show(
-        GameRepository      $gameRepository,
-        TranslatorInterface $translator,
-        string              $slug
+        GameRepository                            $gameRepository,
+        TranslatorInterface                       $translator,
+        string                                    $slug,
+        Request                                   $request,
+        EntityManagerInterface                    $entityManager
     ): Response
     {
         $game = $gameRepository->findOneFullBy($slug);
@@ -26,9 +32,35 @@ final class GameController extends AbstractController
 
         $similarGames = $gameRepository->findBySimilarCategory($game, 3);
 
+        $loggedUser = $this->getUser();
+        $form = null;
+
+        if ($loggedUser !== null) {
+
+            $review = new Review();
+            $form = $this->createForm(ReviewType::class, $review);
+            $form->handleRequest($request);
+
+
+            if ($form->isSubmitted() && $form->isValid()) {
+                $review->setCreatedAt(new \DateTimeImmutable());
+                $review->setUser($loggedUser);
+                $review->setGame($game);
+                $game->addReview($review);
+                $entityManager->persist($review);
+                $entityManager->flush();
+
+                $this->addFlash('success', 'Commentaire ajouté !');
+                return $this->redirectToRoute('app_game_show', ['slug' => $slug]);
+            }
+
+        }
+
+
         return $this->render('front/game/show.html.twig', [
             'game' => $game,
             'similarGames' => $similarGames,
+            'addReviewForm' => $form,
         ]);
     }
 
---
+++ src/Form/ReviewType.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Game;
+use App\Entity\Review;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ReviewType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('content', TextareaType::class, [
+                'label' => 'review.property.content',
+                'attr' => [
+                    'placeholder' => 'review.property.comment',
+                ]
+            ] )
+            ->add('rating', ChoiceType::class, [
+                'label' => 'review.property.rating',
+                'choices' => [
+                    '1' => 1,
+                    '2' => 2,
+                    '3' => 3,
+                    '4' => 4,
+                    '5' => 5,
+                ],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Review::class,
+        ]);
+    }
+}
